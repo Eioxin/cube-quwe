5e250aa68fff37a1b7252cd218da80390a2ab53f
Uwe Wiemer <hallowatcher@gmail.com>
2017-11-23 11:44:16 +0100

    Added watcher for server

--- server/src/routes/account.ts
+++ server/src/routes/account.ts
@@ -4,19 +4,19 @@ import { AccountsService } from '../services/accounts.service';
 const account = Router();
 
 account.get('/', (req, res) => {
-  return AccountsService.getAllAccounts();
+  return res.json(AccountsService.getAllAccounts());
 });
 
 account.post('/', (req, res) => {
-  return AccountsService.createAccount('random', '123');
+  return res.json(AccountsService.createAccount('random', '123'));
 });
 
 account.put('/', (req, res) => {
-  return AccountsService.editAccount('random');
+  return res.json(AccountsService.editAccount('random'));
 });
 
 account.delete('/', (req, res) => {
-  return AccountsService.deleteAccount('random');
+  return res.json(AccountsService.deleteAccount('random'));
 });
 
 export default account;
