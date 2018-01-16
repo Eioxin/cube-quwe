8cbf4883904c8e5bbe50528c04aa940602e6d18b
Uwe Wiemer <hallowatcher@gmail.com>
2018-01-16 14:56:09 +0100

    Filled service

--- client/src/app/shared/services/stationruns.service.ts
+++ client/src/app/shared/services/stationruns.service.ts
@@ -3,13 +3,47 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { map } from 'rxjs/operators';
 
+export class StationRun {
+  id: string;
+  ownerId: string;
+  started = false;
+  players: { [key: string]: Player } = {};
+  stations: { [key: string]: Station } = {};
+}
+
+export class Player {
+  id: string;
+}
+
+export class Station {
+  id: string;
+}
+
 @Injectable()
 export class StationRunService {
   constructor(private _http: Http) { }
 
-  createStationRun(): Observable<string> {
+  createStationRun(): Observable<StationRun> {
     return this._http.get('https://us-central1-cube-quwe.cloudfunctions.net/stationruns/create').pipe(
-      map((response: Response) => response.json().id)
+      map((response: Response) => response.json())
+    );
+  }
+
+  joinStationRun(id: string): Observable<Player> {
+    return this._http.get(`https://us-central1-cube-quwe.cloudfunctions.net/stationruns/${id}/join`).pipe(
+      map((response: Response) => response.json())
+    );
+  }
+
+  createStation(id: string): Observable<Station> {
+    return this._http.get(`https://us-central1-cube-quwe.cloudfunctions.net/stationruns/${id}/station/create`).pipe(
+      map((response: Response) => response.json())
+    );
+  }
+
+  removeStation(stationRunId: string, stationId: string): Observable<Station> {
+    return this._http.get(`https://us-central1-cube-quwe.cloudfunctions.net/stationruns/${stationRunId}/station/${stationId}/remove`).pipe(
+      map((response: Response) => response.json())
     );
   }
 
