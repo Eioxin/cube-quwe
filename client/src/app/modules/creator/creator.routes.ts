07c2900cb10262807aeb93414d5629755f6f254f
Mirko Knorr <mknorr@multamedio.de>
2018-01-25 10:54:35 +0100

    clear it

--- client/src/app/modules/creator/creator.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.page';
-
-export const routes: Routes = [
-    { path: 'landing', component: LandingComponent }
-];
--- client/src/app/modules/home/home.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.page';
-
-export const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent }
-];
